1e5eed32b42939ae08657d226e8b537edc180453
teddy1565 <teddy1565@gmail.com>
2022-01-17 22:30:52 +0800

    矩陣相乘

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Node GPU Calcu Testing
+
+## Intro
+
+測試GPU.js進行平行運算試驗
---
+++ index.js
@@ -0,0 +1,19 @@
+const {GPU} = require("gpu.js");
+const gpu = new GPU();
+
+function normalMatrix(a,b) {
+    const outputLength = a.length*b.length;
+    const multiplyMatrix = gpu.createKernel(function (a, b) {
+        let sum = 0;
+        const ThePowerOfN = 2;
+        for (let i = 0; i < 2**ThePowerOfN; i++) {
+            sum += a[this.thread.y][i] * b[i][this.thread.x];
+        }
+        return sum;
+    }).setOutput([outputLength]);
+
+    return multiplyMatrix(a,b);
+}
+
+let result = normalMatrix([1,2],[3,4]);
+console.log(result)
